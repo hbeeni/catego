33501e142f95922c4a8c3b06b14885db33b0ec33
hbeeni <hyebeen.dev@gmail.com>
2024-01-17 00:54:22 +0900

    Repository 작성
    각 `Entity`의 `JpaRepository`를 생성함.

---
+++ src/main/java/com/been/catego/repository/FolderSubscriptionRepository.java
@@ -0,0 +1,7 @@
+package com.been.catego.repository;
+
+import com.been.catego.domain.FolderSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderSubscriptionRepository extends JpaRepository<FolderSubscription, Long> {
+}
