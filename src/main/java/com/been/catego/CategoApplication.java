ae6360a1079474657d7f66100c0abcbec5695b66
hbeeni <hyebeen.dev@gmail.com>
2024-01-16 18:05:39 +0900

    Spring Boot 프로젝트 시작
    디펜던시:
    - `Spring Web`
    - `Spring Data JPA`
    - `MySQL Driver`
    - `H2 Database`
    - `Thymeleaf`
    - `Spring Security`
    - `OAuth2 Client`
    - `Lombok`
    - `Spring Boot DevTools`

---
+++ src/main/java/com/been/catego/CategoApplication.java
@@ -0,0 +1,13 @@
+package com.been.catego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoApplication.class, args);
+	}
+
+}
