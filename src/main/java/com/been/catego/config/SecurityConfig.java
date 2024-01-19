078a7f911076441d40586c6839efb51671d5c89a
hbeeni <hyebeen.dev@gmail.com>
2024-01-20 05:30:09 +0900

    구글 로그인 구현
    `logoutSuccessUrl`을 설정하지 않으면 로그아웃 시 `/login?logout`으로 리다이렉트됨.
    그래서 이 상태에서 다시 로그인을 하면 `/login?logout`으로 리다이렉트 되어 계속 로그아웃이 되는 상황이 발생함.
    따라서 `logoutSuccessUrl`을 `/login`으로 설정함.

---
+++ src/main/java/com/been/catego/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.been.catego.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String loginURI = "/login";
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage(loginURI)
+                        .permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage(loginURI)
+                        .permitAll()
+                )
+                .logout(logout -> logout.logoutSuccessUrl(loginURI))
+                .build();
+    }
+}
---
+++ src/main/java/com/been/catego/dto/PrincipalDetails.java
@@ -0,0 +1,108 @@
+package com.been.catego.dto;
+
+import com.been.catego.domain.User;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Getter
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private String loginId;
+    private String providerId;
+    private String nickname;
+    private Map<String, Object> oAuth2Attributes;
+
+    public static PrincipalDetails of(String loginId, String providerId, String nickname) {
+        return PrincipalDetails.of(loginId, providerId, nickname, Map.of());
+    }
+
+    public static PrincipalDetails of(String loginId, String providerId, String nickname,
+                                      Map<String, Object> oAuth2Attributes) {
+        return new PrincipalDetails(loginId, providerId, nickname, oAuth2Attributes);
+    }
+
+    public static PrincipalDetails from(User user) {
+        return PrincipalDetails.of(user.getLoginId(), user.getProviderId(), user.getNickname());
+    }
+
+    public static PrincipalDetails from(User user, Map<String, Object> oAuth2Attributes) {
+        return PrincipalDetails.of(user.getLoginId(), user.getProviderId(), user.getNickname(), oAuth2Attributes);
+    }
+
+    private PrincipalDetails(String loginId, String providerId, String nickname, Map<String, Object> oAuth2Attributes) {
+        this.loginId = loginId;
+        this.providerId = providerId;
+        this.nickname = nickname;
+        this.oAuth2Attributes = oAuth2Attributes;
+    }
+
+    public String getProfileImageUrl() {
+        return (String) oAuth2Attributes.get("picture");
+    }
+
+    @Override
+    public String getName() {
+        return loginId;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of(new SimpleGrantedAuthority(RoleType.USER.getRoleName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public enum RoleType {
+
+        USER("ROLE_USER");
+
+        @Getter
+        private final String roleName;
+
+        RoleType(String roleName) {
+            this.roleName = roleName;
+        }
+    }
+}
