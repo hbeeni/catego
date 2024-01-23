773561d69ff1a0bc1d8cc3cc52eae00174e59144
hbeeni <hyebeen.dev@gmail.com>
2024-01-24 07:51:19 +0900

    구독 채널 목록 가져오기 기능 구현
    `YouTubeService`에서 API 요청한 결과를 기반으로 구독 채널 목록 정보를 반환하는 기능을 구현함.
    컨트롤러에서는 구독 채널 목록과 페이지 토큰 정보를 뷰에 보내줌.
    
    사용자의 `id`가 필요해서 `PrincipalDetails`에 `id` 필드를 추가함.

--- src/main/java/com/been/catego/controller/HomeController.java
+++ src/main/java/com/been/catego/controller/HomeController.java
@@ -1,18 +1,36 @@
 package com.been.catego.controller;
 
+import com.been.catego.dto.ChannelResponse;
+import com.been.catego.dto.CommonResponse;
 import com.been.catego.dto.PrincipalDetails;
+import com.been.catego.service.FolderChannelService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final FolderChannelService folderChannelService;
+
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                       @RequestParam(required = false) String pageToken,
+                       Model model) {
+        CommonResponse<List<ChannelResponse>> result = folderChannelService.findAllSubscription(
+                principalDetails.getId(), pageToken);
+
         model.addAttribute("profileImageUrl", principalDetails.getProfileImageUrl());
         model.addAttribute("nickname", principalDetails.getNickname());
+        model.addAttribute("channels", result.getData());
+        model.addAttribute("pageToken", result.getPageToken());
+
         return "home";
     }
 
