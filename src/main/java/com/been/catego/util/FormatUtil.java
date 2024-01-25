f9077d767ea0d3ecb75ecc3e8b0c4fea229f84f7
hbeeni <hyebeen.dev@gmail.com>
2024-01-26 07:07:01 +0900

    Util 클래스 생성 및 수정
    `ConvertUtil`:
    google client의 `DateTime`을 java의 `LocalDateTime`으로 변환하는 로직 추가
    
    `FormatUtil`:
    조회수 포맷, 날짜 포맷 하는 로직 추가

--- src/main/java/com/been/catego/util/FormatUtil.java
+++ src/main/java/com/been/catego/util/FormatUtil.java
@@ -1,7 +1,11 @@
 package com.been.catego.util;
 
+import com.google.api.client.util.DateTime;
+
 import java.math.BigInteger;
 import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public abstract class FormatUtil {
 
@@ -9,10 +13,44 @@ public abstract class FormatUtil {
     }
 
     public static String formatSubscriberCount(BigInteger subscriberCount) {
-        double n = subscriberCount.doubleValue();
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return formatCount(subscriberCount, "#.##");
+    }
 
-        if (n > 10000) {
+    public static String formatViewCount(BigInteger viewCount) {
+        return formatCount(viewCount, "#.#");
+    }
+
+    public static String formatDateTime(DateTime dateTime) {
+        LocalDateTime koreaLDT = ConvertUtil.convertToKoreaLocalDateTime(dateTime);
+        LocalDateTime now = LocalDateTime.now();
+        long minutes = ChronoUnit.MINUTES.between(koreaLDT, now);
+        long hours = ChronoUnit.HOURS.between(koreaLDT, now);
+        long days = ChronoUnit.DAYS.between(koreaLDT, now);
+        long months = ChronoUnit.MONTHS.between(koreaLDT, now);
+        long years = ChronoUnit.YEARS.between(koreaLDT, now);
+
+        if (years > 0) {
+            return years + "년 전";
+        } else if (months > 0) {
+            return months + "개월 전";
+        } else if (days > 0) {
+            return days + "일 전";
+        } else if (hours > 0) {
+            return hours + "시간 전";
+        } else if (minutes > 0) {
+            return minutes + "분 전";
+        } else {
+            return "방금 전";
+        }
+    }
+
+    private static String formatCount(BigInteger count, String format) {
+        double n = count.doubleValue();
+        DecimalFormat decimalFormat = new DecimalFormat(format);
+
+        if (n > 100000000) {
+            return decimalFormat.format(n / 100000000) + "억";
+        } else if (n > 10000) {
             return decimalFormat.format(n / 10000) + "만";
         } else if (n > 1000) {
             return decimalFormat.format(n / 1000) + "천";
